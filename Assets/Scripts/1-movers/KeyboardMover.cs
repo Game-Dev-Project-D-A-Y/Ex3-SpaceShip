4011a08b90a361a41f4b930ed7bd0eaec62c1e82
alonp13 <alonp13@gmail.com>
2020-11-15 14:55:33 +0200

    Finish borders

--- Assets/Scripts/1-movers/KeyboardMover.cs
+++ Assets/Scripts/1-movers/KeyboardMover.cs
@@ -9,17 +9,33 @@ using UnityEngine;
 public class KeyboardMover: MonoBehaviour {
     [Tooltip("Speed of movement, in meters per second")]
     [SerializeField] float speed = 1f;
+    [SerializeField] Rigidbody2D rb;
 
     void Update() {
         float horizontal = Input.GetAxis("Horizontal"); // +1 if right arrow is pushed, -1 if left arrow is pushed, 0 otherwise
         float vertical = Input.GetAxis("Vertical");     // +1 if up arrow is pushed, -1 if down arrow is pushed, 0 otherwise
         Vector3 movementVector = new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
-        transform.position += movementVector;
+        //transform.position += movementVector;
+        rb.velocity = new Vector3(horizontal, vertical, 0) * speed;
         //transform.Translate(movementVector);
         // NOTE: "Translate(movementVector)" uses relative coordinates - 
         //       it moves the object in the coordinate system of the object itself.
         // In contrast, "transform.position += movementVector" would use absolute coordinates -
         //       it moves the object in the coordinate system of the world.
         // It makes a difference only if the object is rotated.
+    }
+
+    public void setSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+
+    public float getSpeed()
+    {
+        return speed;
+    }
+    public void setPosition(Vector3 pos)
+    {
+        transform.position = pos;
     }
 }
