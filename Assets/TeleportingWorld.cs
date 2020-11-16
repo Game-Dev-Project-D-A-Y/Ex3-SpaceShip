117e0477f41ec819a9e54162a87ef4147c86cb8f
Yishay Garame <54310381+YishayGarame@users.noreply.github.com>
2020-11-16 10:47:55 +0200

    Adding more Teleporing

--- Assets/TeleportingWorld.cs
+++ Assets/TeleportingWorld.cs
@@ -11,23 +11,17 @@ public class TeleportingWorld: MonoBehaviour {
     [Tooltip("Speed of movement, in meters per second")]
     [SerializeField] float speed = 1f;
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] string triggeringTag;
 
-    void Update() {
-        float horizontal = Input.GetAxis("Horizontal"); // +1 if right arrow is pushed, -1 if left arrow is pushed, 0 otherwise
-        float vertical = Input.GetAxis("Vertical");     // +1 if up arrow is pushed, -1 if down arrow is pushed, 0 otherwise
-        Vector3 movementVector = new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
-        //transform.position += movementVector;
-        if(this.transform.position.x == 5)
-        {
-            this.transform.position = new Vector3(1,1,0);
+
+
+
+           private void OnTriggerEnter2D(Collider2D other) {
+                if (other.tag == triggeringTag && enabled) {
+                    
         }
-       // rb.velocity = new Vector3(horizontal, vertical, 0) * speed; // it'll make the Object go smoothly and not "jump" againts the wall
-        //transform.Translate(movementVector);
-        // NOTE: "Translate(movementVector)" uses relative coordinates - 
-        //       it moves the object in the coordinate system of the object itself.
-        // In contrast, "transform.position += movementVector" would use absolute coordinates -
-        //       it moves the object in the coordinate system of the world.
-        // It makes a difference only if the object is rotated.
+    
+
     }
 
 
