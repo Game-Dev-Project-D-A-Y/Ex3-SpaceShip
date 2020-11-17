04d5f9dacdd4bf522a685f456bdfa07cf2eae4ae
alonp13 <alonp13@gmail.com>
2020-11-17 16:04:17 +0200

    Bomb added

---
+++ Assets/DestroyAllObjectsOnTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAllObjectsOnTrigger : MonoBehaviour
+{
+    [
+        Tooltip(
+            "Every object tagged with this tag will trigger the destruction of this object")
+    ]
+    [SerializeField]
+    string triggeringTag;
+
+    [SerializeField] NumberField scoreField;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && enabled)
+        {
+            Debug.Log("OnTrigger");
+            DestroyAllObjects();
+            Destroy(this.gameObject);
+        }
+    }
+
+    void DestroyAllObjects()
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(triggeringTag);
+
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            Destroy(gameObjects[i]);
+            scoreField.SetNumber(scoreField.GetNumber() + 1);
+        }
+    }
+}
